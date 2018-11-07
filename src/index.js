f07885ca00e596b797b8cb270dc9c1d025c23fa3
unknown <development.4reg.2qt3@gmail.com>
2018-11-07 21:59:19 +0900

    update,add
    update / index.jsの全体の修正
    add / handleEvent.jsの新規作成

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const line = require('@line/bot-sdk');
 const express = require('express');
-const bodyParser = require('body-parser');
 
 const config = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
@@ -10,28 +9,8 @@ const config = {
 
 const app = express();
 
-/*
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
-*/
-
-const client = new line.Client(config);
-
-const handleEvent = event => {
-    if (event.type !== 'message' || event.message.type !== 'text') {
-        return Promise.resolve(null);
-    }
-
-    return client.replyMessage(event.replyToken, {
-        type: 'text',
-        text: event.message.text
-    });
-}
-
-app.post('/webhook', line.middleware(config), (req, res) => {
-    Promise
-    .all(req.body.events.map(handleEvent))
-    .then(result => res.json(result));
+app.get('/webhook', (req, res) => {
+    console.log(req);
 });
 
 app.listen(process.env.PORT||8080);
